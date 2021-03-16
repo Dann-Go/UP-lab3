a29b9e8dc2f55d7a88388b36df59a37fb5d91c1d
Dann-Go <dan6679990@gmail.com>
2021-03-16 14:30:09 +0300

    done

--- lab/src/Astroid.java
+++ lab/src/Astroid.java
@@ -1,17 +1,22 @@
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+import java.io.Serializable;
 
-public class Astroid implements Shape {
+public class Astroid implements Shape, Cloneable, Transferable, Serializable {
 
     private final int param ;
     private final double delta = 0.001;
     private final double startAngle = -Math.PI / 2 + delta;
     private final double endAngle = startAngle + 2 * Math.PI;
-    private final float centerX ;
-    private final float centerY ;
+    private float centerX ;
+    private float centerY ;
 
     public Astroid(int param, int centerX, int centerY) {
         this.param = param;
@@ -21,42 +26,63 @@ public class Astroid implements Shape {
 
     @Override
     public Rectangle getBounds() {
-        return null;
+        return new Rectangle(0, 0, (int)centerX*2, (int)centerY*2);
     }
 
     @Override
     public Rectangle2D getBounds2D() {
-        return null;
+        return new Rectangle2D.Float(0, 0, centerX*2, centerY*2);
+    }
+
+    public int getCenterX() {
+        return (int)centerX;
+    }
+
+    public int getCenterY() {
+        return (int)centerY;
+    }
+
+    public double getR(double alpha) {
+        return alpha*130;
     }
 
     @Override
     public boolean contains(double x, double y) {
-        return false;
+        if (!getBounds().contains(x, y))
+            return false;
+        double x0 = x - getCenterX();
+        double y0 = getCenterY() - y;
+        if (x0 < 0 || y0 < 0)
+            return false;
+        double alpha = Math.atan(y0 / x0);
+        double r = getR(alpha);
+        return x0 * x0 + y0 * y0 < r * r;
     }
 
     @Override
     public boolean contains(Point2D p) {
-        return false;
+        return contains(p.getX(), p.getY());
     }
 
     @Override
     public boolean intersects(double x, double y, double w, double h) {
-        return false;
+        return getBounds().intersects(x, y, w, h);
     }
 
     @Override
     public boolean intersects(Rectangle2D r) {
-        return false;
+        return getBounds().intersects(r);
     }
 
     @Override
     public boolean contains(double x, double y, double w, double h) {
-        return false;
+        return contains(x, y) && contains(x + w, y) && contains(x, y + h)
+                && contains(x + w, y + h);
     }
 
     @Override
     public boolean contains(Rectangle2D r) {
-        return false;
+        return contains(r.getX(), r.getY(), r.getWidth(), r.getHeight());
     }
 
     @Override
@@ -67,6 +93,11 @@ public class Astroid implements Shape {
     @Override
     public PathIterator getPathIterator(AffineTransform at, double flatness) {
         return new PlotIterator(at, flatness);
+    }
+
+    public void translate(double x, double y) {
+        centerX += x;
+        centerY += y;
     }
 
 
@@ -131,6 +162,53 @@ public class Astroid implements Shape {
             if (angle == startAngle) return SEG_MOVETO;
             else return SEG_LINETO;
         }
+    }
+    static DataFlavor decDataFlavor = new DataFlavor(Astroid.class, "Witch_of_Agnesi");
+
+    // This is a list of the flavors we know how to work with
+    private static DataFlavor[] supportedFlavors = {decDataFlavor, DataFlavor.stringFlavor};
+
+
+    @Override
+    public DataFlavor[] getTransferDataFlavors() {
+        return supportedFlavors.clone();
+    }
+
+    @Override
+    public boolean isDataFlavorSupported(DataFlavor dataFlavor) {
+        return (dataFlavor.equals(decDataFlavor) || dataFlavor.equals(DataFlavor.stringFlavor));
+    }
+
+    @Override
+    public Object getTransferData(DataFlavor dataFlavor) throws UnsupportedFlavorException, IOException {
+        if (dataFlavor.equals(decDataFlavor)) {
+            return this;
+        } else if (dataFlavor.equals(DataFlavor.stringFlavor)) {
+            return toString();
+        } else
+            throw new UnsupportedFlavorException(dataFlavor);
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) { // This should never happen
+            return this;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return param + " " + centerX + " " + centerY ;
+    }
+
+
+    static Astroid getFromString(String line) {
+        String[] arr = line.split(" ");
+        return new Astroid(Integer.parseInt(arr[0]),
+                Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
     }
 
 }
